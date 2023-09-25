09e42390c17954e852d2f243f829fa56ce8a1c9b
belgrades <fcremaldc@gmail.com>
2023-09-25 16:37:50 +0200

    Adding all

---
+++ .tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,180 @@
+// .tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: "0d7f70e7-0fdd-4300-b51b-5295bbbb3fa7",
+  // Get this from tina.io
+  token: "912fa64962ca2de446241a9b7ee24054af19c1a6",
+  // Get this from tina.io
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Homepage",
+        name: "homepage",
+        path: "content/en",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        format: "md",
+        label: "Introduction",
+        name: "introduction",
+        path: "content/en/1 Introduction",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        format: "md",
+        label: "Usage",
+        name: "usage",
+        path: "content/en/2 Usage",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        format: "md",
+        label: "Functions",
+        name: "functions",
+        path: "content/en/3 Functions",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        format: "md",
+        label: "P\xE1gina de inicio",
+        name: "p_gina_de_inicio",
+        path: "content/es",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        format: "md",
+        label: "Introducci\xF3n",
+        name: "introducci_n",
+        path: "content/es/1 Introducci\xF3n",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        format: "md",
+        label: "Uso",
+        name: "uso",
+        path: "content/es/2 Uso",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        format: "md",
+        label: "Funciones",
+        name: "funciones",
+        path: "content/es/3 Funciones",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*"
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
