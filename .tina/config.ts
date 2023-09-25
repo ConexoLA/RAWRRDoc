7ddfac66f9d7e18d1baa3a7b115ae6964bd8065b
belgrades <fcremaldc@gmail.com>
2023-09-25 13:45:25 +0200

    Adding search

--- .tina/config.ts
+++ .tina/config.ts
@@ -98,4 +98,12 @@ export default defineConfig({
       },
     ],
   },
+  search: {
+    tina: {
+      indexerToken: '20dec594e422fe1e56e7bbe3d2331f2ba2e18885',
+      stopwordLanguages: ['eng', 'es']
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100
+  },
 });
